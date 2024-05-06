088553806c9769da1c2b7a7efc6dd787c95e32fe
samkallon <sjlsamkallon@gmail.com>
2024-05-06 14:56:04 +0800

    调整展示方式 为新建html

--- src/components/demos/randomUpLine/upLine.vue
+++ src/components/demos/randomUpLine/upLine.vue
@@ -9,8 +9,9 @@ onMounted(async () => {
   const samCzm = new SamCesiumUtils.samCzm({Cesium:Cesium})
   samCzm.initViewer({id:'cesiumContainer', initCamera:false})
   viewer = samCzm.viewer
+  const cartesianCenter = new Cesium.BoundingSphere(Cesium.Cartesian3.fromDegrees(center[0],center[1]), 3000)
   viewer.scene.camera.flyToBoundingSphere(
-      new Cesium.BoundingSphere(Cesium.Cartesian3.fromDegrees(center[0],center[1]), 3000),
+      cartesianCenter,
       {
         offset: new Cesium.HeadingPitchRoll(0, Cesium.Math.toRadians(-30), 0),
         duration: 1
@@ -18,6 +19,11 @@ onMounted(async () => {
   )
   // 随机竖直飞线
   samCzm.lineFlowInit(center, 200);
+  const transform = Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(center[0],center[1]))
+  viewer.scene.camera.lookAtTransform(transform,new Cesium.HeadingPitchRange(0,-Math.PI / 8,2900))
+  viewer.clock.onTick.addEventListener((clock)=>{
+    viewer.scene.camera.rotateRight(0.003)
+  })
 })
 
 const curr = ref(1)
