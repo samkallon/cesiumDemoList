a26b9a082b81d7552e2a4857e6ca3dad02232f9d
samkallon <sjlsamkallon@gmail.com>
2024-10-16 09:59:40 +0800

    添加天际线

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/cesiumDemoList/',
+  base: './',
   root: './',
   server:{
     host:'0.0.0.0',
